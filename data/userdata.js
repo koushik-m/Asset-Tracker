8b224fa25dcf89fe78172e8797b68aaabb95d47d
koushik-m <koushik2498@gmail.com>
2019-04-09 09:50:33 +0530

    refactored code

---
+++ data/userdata.js
@@ -0,0 +1,18 @@
+var userdata = [{
+    name: "John",
+    username: "John",
+    email: "john@gmail.com",
+    password: "john12"
+},
+{
+    name: "Grisham",
+    username: "Grisham",
+    email: "grisham@gmail.com",
+    password: "grisham12"
+},
+{
+    name: "peter",
+    username: "peter",
+    email: "peter@gmail.com",
+    password: "peter12"
+}];
